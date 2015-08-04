ea5b21931386f463b2945a4913cf87b30b866f15
parsonsc <chris.parsons@goodagency.co.uk>
2015-08-04 17:25:39 +0100

    updates

--- js/main.js
+++ js/main.js
@@ -21,3 +21,35 @@ function centerModal() {
 	$(window).on("resize", function() {
 		$('.modal:visible').each(centerModal);
 	});
+$(function(){
+  var $searchlink = $('#searchtoggl i');
+  var $searchbar  = $('#searchbar');
+  
+  $('li.search').on('click', function(e){
+    e.preventDefault();
+    
+    if($(this).attr('id') == 'searchtoggl') {
+      // if(!$searchbar.is(":visible")) { 
+      //   // if invisible we switch the icon to appear collapsable
+      //   $searchlink.removeClass('fa-search').addClass('fa-search-minus');
+      // } else {
+      //   // if visible we switch the icon to appear as a toggle
+      //   $searchlink.removeClass('fa-search-minus').addClass('fa-search');
+      // }
+      
+      $searchbar.slideToggle(300, function(){
+        // callback after search bar animation
+      });
+    }
+  });
+  
+  $('#searchform').submit(function(e){
+    e.preventDefault(); // stop form submission
+  });
+});
+$(document).ready(function(){
+	$(".nav_button").click(function(){
+		$("nav.site_nav").slideToggle();
+
+	}); 
+});
